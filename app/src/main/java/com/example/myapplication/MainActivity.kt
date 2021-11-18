36b4a05f213d26201a3fad7fa2fd2413f07c66b7
user <frostkslo1@yandex.ru>
2021-11-18 21:33:09 +0300

    reg and log design done

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    public fun reg(v: View){
+        var login = findViewById<EditText>(R.id.login_et).text.toString()
+        val intent = Intent(this, RegActivity::class.java).putExtra("login", login)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/myapplication/RegActivity.kt
@@ -0,0 +1,61 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.TextView
+import java.util.regex.Pattern
+
+class RegActivity : AppCompatActivity() {
+    var EMAIL_ADDRESS_PATTERN: Pattern = Pattern.compile(
+        "[a-z0-9]{1,256}" +
+                "\\@" +
+                "[a-z0-9]{1,64}" +
+                "\\." +
+                "[a-z]{1,3}"
+    )
+    var email_et: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reg)
+        val login = intent.getStringExtra("login")
+        findViewById<EditText>(R.id.login_et).setText(login)
+        this.email_et = findViewById<EditText>(R.id.email_et)
+        email_et?.setOnFocusChangeListener { view, v -> checkMail(v) }
+        val sec_pass = findViewById<EditText>(R.id.password_et2)
+        val first_pass = findViewById<EditText>(R.id.password_et)
+        val textWatcher = object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable?) {
+                if (sec_pass.text.toString() != first_pass.text.toString() && first_pass.text.toString() != "" && sec_pass.text.toString() != "") {
+                    sec_pass.error = "Password mismatch"
+                    first_pass.error = "Password mismatch"
+                } else{
+                    sec_pass.error = null
+                    first_pass.error = null
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+        }
+        sec_pass.addTextChangedListener(textWatcher)
+        first_pass.addTextChangedListener(textWatcher)
+    }
+
+    private fun checkMail(b: Boolean) {
+        if (!b && email_et?.text != "" && !EMAIL_ADDRESS_PATTERN.matcher(email_et?.text.toString())
+                .matches()
+        ) {
+            email_et?.error = "Please write normal email"
+        } else {
+            email_et?.error = null
+        }
+    }
+}
