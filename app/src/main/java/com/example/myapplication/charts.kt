796b52353b19273effec13c1f0cec802f8e4fe1f
pankov <frostkslo1@yandex.ru>
2022-01-11 13:58:20 +0300

    2 задание выполнено, единственное что можно заметить это что первая диограма отображается ниже, но я без понятия как это можно исправить.

--- app/src/main/java/com/example/myapplication/charts.kt
+++ app/src/main/java/com/example/myapplication/charts.kt
@@ -1,19 +1,24 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.RadioButton
 import android.widget.RadioGroup
+import android.widget.TextView
 import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+import org.w3c.dom.Text
 import kotlin.random.Random
 
 class charts : AppCompatActivity() {
      lateinit var chart: BarChart
+     lateinit var descTextView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_charts)
@@ -24,6 +29,7 @@ class charts : AppCompatActivity() {
                 modifyData_second()
             }
         }
+        descTextView = findViewById(R.id.description)
         chart = findViewById(R.id.chart)
         chart.xAxis.position = XAxis.XAxisPosition.BOTTOM_INSIDE
         chart.axisLeft.setDrawLabels(false)
@@ -51,9 +57,11 @@ class charts : AppCompatActivity() {
             index += 1
         }
         chart.xAxis.valueFormatter = IndexAxisValueFormatter(numbers_for_labels)
+        var description: String = ""
         for (i in 0..4) {
-            chart.description.text += i.toString() + " - " + names[i] + "\n"
+            description += i.toString() + " - " + names[i] + "\n"
         }
+        descTextView.text = description
         //chart.description.setPosition(3f,-3f)
         val dataset = BarDataSet(data, "BarDataSet")
         dataset.valueTextSize = 13f
@@ -77,13 +85,20 @@ class charts : AppCompatActivity() {
             index += 1
         }
         chart.xAxis.valueFormatter = IndexAxisValueFormatter(numbers_for_labels)
+        var description: String = ""
         for (i in 0..4) {
-            chart.description.text += i.toString() + " - " + names[i] + "\n"
+            description += i.toString() + " - " + names[i] + "\n"
         }
+        descTextView.text = description
         //chart.description.setPosition(3f,-3f)
         val dataset = BarDataSet(data, "BarDataSet")
         dataset.valueTextSize = 13f
         chart.data = BarData(dataset)
         chart.invalidate()
+    }
+    
+    fun backToCountries(v: View){
+        // startActivity(Intent(this, CountriesActivity::class.java))
+        this.finish()
     }
 }
